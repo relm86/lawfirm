27120873e26ace8ea8396c80e85cb59dbafba749
blackmonk88 <blackmonk88@gmail.com>
2014-10-16 23:46:22 +0700

    foursquare widget update attempt 4th

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -528,8 +528,26 @@ class Dashboard extends CI_Controller {
 				return serialize($twitter);
 			endif;
 		
-		require_once(APPPATH.'widgets/foursquare/dashboard_get_widget_data.php');
-		
+		//require_once(APPPATH.'widgets/foursquare/dashboard_get_widget_data.php');
+		elseif ( 'foursquare' == $this->input->post('widget_type')):
+			$foursquare_title = $this->input->post('foursquare-title');
+			$foursquare_hashtag = $this->input->post('foursquare-hashtag');
+			$i = 1;
+			if(strlen($foursquare_title[1])==0) {
+					$foursquare_title[1] = 'Twitter Feed';
+					$foursquare_title[1] = 'autoaccident';
+			} else {
+				foreach($foursquare_title as $title):
+					$foursquare[$i]['title'] = $title;
+					$foursquare[$i]['hashtag'] = $foursquare_hashtag[$i];
+					$i++;
+				endforeach;
+			}
+			//print_r($foursquare);
+			if ( isset($foursquare) && count($foursquare) > 0 ):
+				return serialize($foursquare);
+			endif;			
+
 		endif;
 		return FALSE;
 	}
