fae0103fff1f55b06547c700b75aeda2a83eec13
blackmonk88 <blackmonk88@gmail.com>
2014-10-18 00:54:48 +0700

    edit interface

--- application/views/dashboard/user/edit.php
+++ application/views/dashboard/user/edit.php
@@ -11,7 +11,7 @@
                     </div>
                     <?php echo form_open('dashboard/edituser/'.$row->id, array('class' => 'edituser_form', 'id' => 'edituser_form') ); ?>
                         <div id="edituser_button_top">
-                            <a href="<?php echo site_url(); ?>" class="btn btn-default">Cancel</a> &nbsp; <button type="submit" class="btn btn-success">Save</button>
+                            &nbsp;<a href="<?php echo site_url().'/dashboard/'; ?>" class="btn btn-default">Cancel</a> <button type="submit" class="btn btn-success">Save</button>
                         </div>
                         <?php foreach ($row as $key => $value): ?>
                             <?php if ($key == 'picture'): ?>
@@ -39,7 +39,7 @@
                                 <?php break; ?>
                             <?php endif; ?>
                         <?php endforeach; ?>
-                        <a href="<?php echo site_url(); ?>" class="btn btn-default">Cancel</a> &nbsp; <button type="submit" class="btn btn-success">Save</button>
+                        &nbsp;<a href="<?php echo site_url().'/dashboard/'; ?>" class="btn btn-default">Cancel</a> <button type="submit" class="btn btn-success">Save</button>
                     <?php echo form_close(); ?>
                 </div>
             </div>
